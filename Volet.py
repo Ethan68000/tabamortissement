a4ee1e0e06c486ebac238eae31fd9e5143948f8e
tabamortissement <cathycoulon910@gmail.com>
2023-05-04 14:42:38 +0200

    modifier

--- Volet.py
+++ Volet.py
@@ -2,3 +2,4 @@ diametre= int(input("Entrez le diametre de l'axe en mm : "))
 tours= int(input("Entrez le nombre de tours : "))
 
 print("Calcul de la Longueur L pour chaque tour :")
+print("Calcul de la Longueur L par formule :")
