7213daa7dcd36dd90c1200f9d5be4c070cd2de2e
tabamortissement <cathycoulon910@gmail.com>
2023-05-04 14:24:15 +0200

    modifier

--- inerets plac.py
+++ inerets plac.py
@@ -4,9 +4,14 @@ taux = float(input("Entrez le taux annuel en % : "))
 annees = int(input("Entrez le nombre d'années : "))
 banque = placement
 
+#for i in range(annees):
+    #banque += 12*versementmen
+    #banque *= (1+taux/100)
+
 for i in range(annees*12):
     banque += versementmen
-    banque += (1+taux/12/100)
+    banque *= (1+taux/12/100)
 
 print(f"Le capital acquis avec intérêts est de {round(banque, 2)} euros au bout de {round(annees)} avec des versements mensuels de {round(versementmen, 2)} euros")
 print(f"Les intérêts gagnés au taux annuel de {round(taux, 2)} % sont de {round(banque - placement - 12*annees*versementmen, 2)}")
+print(f"Sans placement avec interets le capital acquis serait de {round(placement + 12*annees*versementmen)} euros")
