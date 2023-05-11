5c28510c9ebf263fbe8de43b492ff7d65fe8f66a
tabamortissement <cathycoulon910@gmail.com>
2023-05-11 10:40:48 +0200

    modifier

--- Volet.py
+++ Volet.py
@@ -1,5 +1,15 @@
+
+import math
+
 diametre= int(input("Entrez le diametre de l'axe en mm : "))
 tours= int(input("Entrez le nombre de tours : "))
+print("Calcul de la longueur L par formule :")
 
-print("Calcul de la Longueur L pour chaque tour :")
-print("Calcul de la Longueur L par formule :")
+l = 0
+d = diametre
+for i in range(1, tours+1):
+    d += 18
+    l +=math.pi*(d-18)
+    print(f"Tour : {i} - Diametre [mm]: {d} - Longueur enroulée [mm] : {round(l)}")
+
+print(f"Calcul de la longueur L par formule :\nLongueur [mm] pour 10 tours: {l}")
