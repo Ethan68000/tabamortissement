224a1b7fcedca9001846e59b1fc3a70d5e4aeaab
Ethan68000 <118525512+Ethan68000@users.noreply.github.com>
2023-05-25 07:57:44 +0200

    Update Volet.py

--- Volet.py
+++ Volet.py
@@ -14,3 +14,29 @@ for i in range(1, tours+1):
 
 print(f"Calcul de la longueur L par formule :\nLongueur [mm] pour 10 tours: {l}")
 
+
+
+
+
+
+
+import math
+
+def d(n):
+    return (d0+2*9*n)
+    
+d0=float(input("entrer le diametre de l'axe en mm"))
+n=int(input("entrer le nombre de tours n"))
+
+print("Calcul de la longeur L pour chaque tour:")
+L=0
+for i in range(n):
+    L=L+math.pi*d(i+1)
+    print("tour=",i+1,"-""Diametre mm :",round(d(i+1),1),"- Longeur enroulÃ©e [mm]:",round(L))
+    
+print("calcul de la longeur l par formule:")
+c0=math.pi*d(0)
+cn=math.pi*d(n-1)
+L=(c0+cn)*(n+0)/2
+print("Longeur [mm] pour ",n,"tours:", L)
+print 
